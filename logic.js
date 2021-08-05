1d01ea686551a84beee440b5ed091e0ee09ef657
David Mendoza <davidmendoza0101@gmail.com>
2021-08-05 03:12:29 -0700

    Adding completed challenge to the repository

--- logic.js
+++ logic.js
@@ -1,24 +1,214 @@
+$(document).ready(function(){
+//hides the end screen by default 
+$("#end-screen").hide();
+//sets the value of the final score span to 0
+$("#final-score").val(0);
 
+//creates variable for final-score span
+var input = $('#final-score');
+
+//function that on click starts a timer that counts down from 300 seconds
+//It also starts the quiz
 $("#start").click(function() {
-    var count= 10;
+    var count= 300;
     var counter =setInterval(timer, 1000); 
-    
     function timer () {
+        //every 1000 milliseconds the value of the count variable is reduced by 1
         count = count-1;
+        //if the count variable reaches -1 it stops counting down 
         if (count<=-1){
             clearInterval(counter);
     
-             return;
-         }
-    
+            return;
+        }
+        //displays the counter in the #time span
         $("#time").text(count + "secs");
+
+        
     };
 
-    $("#start").hide();
-    $("#questions").removeClass("hide");
+    //when the first question is submitted if the wrong answers are checked reduce the value of the count variable by 10 
+    //this lowers the leftover time by 10 seconds 
+    $("#submit1").click(function() {
+        if ($('#choiceA:checked').length > 0 || $('#choiceB:checked').length > 0 || $('#choiceC:checked').length > 0) {
+            count= count-10;
+        }
+            //if the right answer is checked create a variable newNumber and set it equal to the input value +1
+            else if ($('#choiceD:checked').length>0){
+                var newNumber = Number(input.val()) + 1;
+                //set the input value and text equal to the new total value 
+                input.val(newNumber);
+                input.text(newNumber);
+
+            }
+
+    });
+
+    //same for when question 2 is submitted
+    $("#submit2").click(function() {
+        if ($('#choiceA:checked').length > 0 || $('#choiceB:checked').length > 0 || $('#choiceC:checked').length > 0) {
+            count= count-10;
+        }
+            else if ($('#choiceD:checked').length>0){
+                var newNumber = Number(input.val()) + 1;
+                input.val(newNumber);
+                input.text(newNumber);
+            }
+
+    });
+
+    //same for when question 3 is submitted
+    $("#submit3").click(function() {
+        if ($('#choiceA:checked').length > 0 || $('#choiceB:checked').length > 0 || $('#choiceC:checked').length > 0) {
+            count= count-10;
+        }
+            else if ($('#choiceD:checked').length>0){
+                var newNumber = Number(input.val()) + 1;
+                input.val(newNumber);
+                input.text(newNumber);
+            }
+
+    });
+
+    //same for when question 4 is submitted
+    $("#submit4").click(function() {
+        if ($('#choiceA:checked').length > 0 || $('#choiceB:checked').length > 0 || $('#choiceC:checked').length > 0) {
+            count= count-10;
+        }
+            else if ($('#choiceD:checked').length>0){
+                var newNumber = Number(input.val()) + 1;
+                input.val(newNumber);
+                input.text(newNumber);
+            }
+
+    });
+
+    //same for when question 5 is submitted
+    $("#submit5").click(function() {
+        if ($('#choiceA:checked').length > 0 || $('#choiceB:checked').length > 0 || $('#choiceC:checked').length > 0) {
+            count= count-10;
+        }
+            else if ($('#choiceD:checked').length>0){
+                var newNumber = Number(input.val()) + 1;
+                input.val(newNumber);
+                input.text(newNumber);
+            }
+
+    });
+
+    //same for when question 6 is submitted
+    $("#submit6").click(function() {
+        if ($('#choiceA:checked').length > 0 || $('#choiceB:checked').length > 0 || $('#choiceC:checked').length > 0) {
+            count= count-10;
+        }
+            else if ($('#choiceD:checked').length>0){
+                var newNumber = Number(input.val()) + 1;
+                input.val(newNumber);
+                input.text(newNumber);
+            }
+
+    });
+
+    //same for when question 7 is submitted
+    $("#submit7").click(function() {
+        if ($('#choiceA:checked').length > 0 || $('#choiceB:checked').length > 0 || $('#choiceC:checked').length > 0) {
+            count= count-10;
+        }
+            else if ($('#choiceD:checked').length>0){
+                var newNumber = Number(input.val()) + 1;
+                input.val(newNumber);
+                input.text(newNumber);
+            }
+
+    });
+
+    //same for when question 8 is submitted
+    $("#submit8").click(function() {
+        if ($('#choiceA:checked').length > 0 || $('#choiceB:checked').length > 0 || $('#choiceC:checked').length > 0) {
+            count= count-10;
+        }
+            else if ($('#choiceD:checked').length>0){
+                var newNumber = Number(input.val()) + 1;
+                input.val(newNumber);
+                input.text(newNumber);
+            }
+
+    });
+
+    //same for when question 9 is submitted
+    $("#submit9").click(function() {
+        if ($('#choiceA:checked').length > 0 || $('#choiceB:checked').length > 0 || $('#choiceC:checked').length > 0) {
+            count= count-10;
+        }
+            else if ($('#choiceD:checked').length>0){
+                var newNumber = Number(input.val()) + 1;
+                input.val(newNumber);
+                input.text(newNumber);
+            }
+    
+    });
+    //same for when question 10 is submitted
+    $("#submit10").click(function() {
+        console.log($("input[name=multians]:checked").length);
+        if ($('#choiceA:checked').length > 0 || $('#choiceB:checked').length > 0 || $('#choiceC:checked').length > 0) {
+            count= count-10;
+        }
+            else if ($('#choiceD:checked').length>0){
+                var newNumber = Number(input.val()) + 1;
+                input.val(newNumber);
+                input.text(newNumber);
+                console.log($("#final-score").val());
+            }
+
+    });
+});
+
+//create a highscores variable that gets the parsed highscores initals and puts them in an empty array
+var highscores = JSON.parse(window.localStorage.getItem("highscores")) || [];
+//create a highscoresVal variable that gets the parsed highscore values and puts them in an empty array
+var highscoresVal=JSON.parse(window.localStorage.getItem("highscoresVal")) || [];
+
+//when the submit button on the end screen is clicked 
+$("#submit").click(function(){ 
+    //set the variable initial name to be that value of the initals input
+    var initialName= $('#initials').val();
+    //set the variable initialScore to be the value of the final-score span 
+    var initialScore= $('#final-score').val();
+
+    //hide the paragraphs of the end screen
+    $("#end-screen p").hide();
+
+    //appends a div with the words Score submitted! to the end screen
+    $("#end-screen").append("<div> Score Submitted! </div>");
+
+    //pushes the initals enterd in the #initals input at the end of the highscores array
+    highscores.push(initialName);
+
+    //pushes the score for the inital given to the end of the highscoresVal array 
+    highscoresVal.push(initialScore);
+
+    //stores the highscores initial and score value to local storage
+    localStorage.setItem('highscores', JSON.stringify(highscores));
+    localStorage.setItem('highscoresVal', JSON.stringify(highscoresVal));
 
 });
 
-$("#end-screen").hide();
+    
+});
+//gets the highscore initals from local storage and sets them equal to the highscores array
+var highscores= JSON.parse(localStorage.getItem('highscores'));
+//gets the highscores value from local storage and sets them equal to the highscoresVal array
+var highscoresVal= JSON.parse(localStorage.getItem('highscoresVal'));
 
+//creates loop that for the length of the highscores array appends the highscores ordered list with the highscores and highscoresVal arrays 
+for (var i=0; i< highscores.length; i++) {
+    $("#highscores").append("<li>" + $(highscores)[i] + ":  " + $(highscoresVal)[i] + "</li>");
 
+};
+
+//When the clear highscores button is pressed the localStorage and the ordered list in the highscores page are cleared 
+$("#clear").click(function(){
+    localStorage.clear();
+    $("ol").empty();
+
+});
--- questions.js
+++ questions.js
@@ -1,19 +1,171 @@
+//created variables for the question and choice placeholder html 
 var title = $("#question-title");
 var options = $("#choices");
 
-//$(document).ready(function(){
+$(document).ready(function(){
 
-choiceA= $("#choices").append("<Input type='radio' id ='choiceA' name='multians' value='A'>A) </Input> <br>");
-choiceB= $("#choices").append("<Input type='radio' name='multians' value='B'>B) </Input> <br>");
-choiceC= $("#choices").append("<Input type='radio' name='multians' value='C'>C) </Input> <br>");
-choiceD= $("#choices").append("<Input type='radio' name='multians' value='D'>D) </Input> <br>");
+    //appends html into choices div that adds radio inputs with text next to them.
+    $("#choices").append("<Input type='radio' id ='choiceA' name='multians' value='A'>A) <br> <br>");
+    $("#choices").append("<Input type='radio' id ='choiceB' name='multians' value='B'>B) <br> <br>");
+    $("#choices").append("<Input type='radio' id ='choiceC' name='multians' value='C'>C) <br> <br>");
+    $("#choices").append("<Input type='radio' id ='choiceD' name='multians' value='D'>D) <br> <br>");
 
-choiceA.id = choice1;
-console.log(choiceA);
-//});
+    //Adds buttons that are going to be used to submit answers
+    submitButtonA= $("#questions").append("<button id='submit1'> Submit </button>");
+    submitButtonB= $("#questions").append("<button id='submit2'> Submit </button>");
+    submitButtonC= $("#questions").append("<button id='submit3'> Submit </button>");
+    submitButtonD= $("#questions").append("<button id='submit4'> Submit </button>");
+    submitButtonE= $("#questions").append("<button id='submit5'> Submit </button>");
+    submitButtonF= $("#questions").append("<button id='submit6'> Submit </button>");
+    submitButtonG= $("#questions").append("<button id='submit7'> Submit </button>");
+    submitButtonH= $("#questions").append("<button id='submit8'> Submit </button>");
+    submitButtonI= $("#questions").append("<button id='submit9'> Submit </button>");
+    submitButtonJ= $("#questions").append("<button id='submit10'> Submit </button>");
 
+//Hides the submit buttons before they are used
+    $("#submit2, #submit3, #submit4, #submit5, #submit6, #submit7, #submit8, #submit9, #submit10").hide();
+
+});
+
+//Adds first question when the start button is pressed 
 $("#start").click(function(){
-    title = $("#question-title").text("What is Jquery");
-    options = $("choice1").append("Answer 1");
+    title = $("#question-title").text("What is JQuery?");
+    //hides the start button
+    $("#start").hide();
+    $("#questions").removeClass("hide");
 
-})
+    $('#choiceA')[0].nextSibling.data = 'A) Choice A';
+    $('#choiceB')[0].nextSibling.data = 'B) Choice B';
+    $('#choiceC')[0].nextSibling.data = 'C) Choice C';
+    $('#choiceD')[0].nextSibling.data = 'D) Choice D';
+
+
+});
+//question2
+$(document).ready(function(){
+    $('#submit1').click(function(){
+        title = $("#question-title").text("What is HTML?");
+        $('#choiceA')[0].nextSibling.data = 'A) Choice E';
+        $('#choiceB')[0].nextSibling.data = 'B) Choice F';
+        $('#choiceC')[0].nextSibling.data = 'C) Choice G';
+        $('#choiceD')[0].nextSibling.data = 'D) Choice H';
+        //hides first button and shows the second 
+        $("#submit1").hide();
+        $("#submit2").show();
+
+    });
+});
+//question3
+$(document).ready(function(){
+    $('#submit2').click(function(){
+        title = $("#question-title").text("What is Bootstrap?");
+        $('#choiceA')[0].nextSibling.data = 'A) Choice I';
+        $('#choiceB')[0].nextSibling.data = 'B) Choice J';
+        $('#choiceC')[0].nextSibling.data = 'C) Choice K';
+        $('#choiceD')[0].nextSibling.data = 'D) Choice L';
+        $("#submit2").hide();
+        $("#submit3").show();
+    
+    });
+});
+//question4
+$(document).ready(function(){
+    $('#submit3').click(function(){
+        title = $("#question-title").text("What is Figma?");
+        $('#choiceA')[0].nextSibling.data = 'A) Choice M';
+        $('#choiceB')[0].nextSibling.data = 'B) Choice N';
+        $('#choiceC')[0].nextSibling.data = 'C) Choice O';
+        $('#choiceD')[0].nextSibling.data = 'D) Choice P';
+        $("#submit3").hide();
+        $("#submit4").show();
+    
+    });
+});
+//question5
+$(document).ready(function(){
+    $('#submit4').click(function(){
+        title = $("#question-title").text("What are classes?");
+        $('#choiceA')[0].nextSibling.data = 'A) Choice Q';
+        $('#choiceB')[0].nextSibling.data = 'B) Choice R';
+        $('#choiceC')[0].nextSibling.data = 'C) Choice S';
+        $('#choiceD')[0].nextSibling.data = 'D) Choice T';
+        $("#submit4").hide();
+        $("#submit5").show();
+    
+    });
+});
+//question6
+$(document).ready(function(){
+    $('#submit5').click(function(){
+        title = $("#question-title").text("What are for loops?");
+        $('#choiceA')[0].nextSibling.data = 'A) Choice U';
+        $('#choiceB')[0].nextSibling.data = 'B) Choice V';
+        $('#choiceC')[0].nextSibling.data = 'C) Choice W';
+        $('#choiceD')[0].nextSibling.data = 'D) Choice X';
+        $("#submit5").hide();
+        $("#submit6").show();
+    
+    });
+});
+//question7
+$(document).ready(function(){
+    $('#submit6').click(function(){
+        title = $("#question-title").text("What is javascript?");
+        $('#choiceA')[0].nextSibling.data = 'A) Choice Y';
+        $('#choiceB')[0].nextSibling.data = 'B) Choice Z';
+        $('#choiceC')[0].nextSibling.data = 'C) Choice AA';
+        $('#choiceD')[0].nextSibling.data = 'D) Choice BB';
+        $("#submit6").hide();
+        $("#submit7").show();
+    
+    });
+});
+//question8
+$(document).ready(function(){
+    $('#submit7').click(function(){
+        title = $("#question-title").text("What is an API?");
+        $('#choiceA')[0].nextSibling.data = 'A) Choice CC';
+        $('#choiceB')[0].nextSibling.data = 'B) Choice DD';
+        $('#choiceC')[0].nextSibling.data = 'C) Choice EE';
+        $('#choiceD')[0].nextSibling.data = 'D) Choice FF';
+        $("#submit7").hide();
+        $("#submit8").show();
+    
+    });
+});
+//question9
+$(document).ready(function(){
+    $('#submit8').click(function(){
+        title = $("#question-title").text("What is the console?");
+        $('#choiceA')[0].nextSibling.data = 'A) Choice GG';
+        $('#choiceB')[0].nextSibling.data = 'B) Choice HH';
+        $('#choiceC')[0].nextSibling.data = 'C) Choice II';
+        $('#choiceD')[0].nextSibling.data = 'D) Choice JJ';
+        $("#submit8").hide();
+        $("#submit9").show();
+    
+    });
+});
+//question10
+$(document).ready(function(){
+    $('#submit9').click(function(){
+        title = $("#question-title").text("What is the DOM?");
+        $('#choiceA')[0].nextSibling.data = 'A) Choice KK';
+        $('#choiceB')[0].nextSibling.data = 'B) Choice LL';
+        $('#choiceC')[0].nextSibling.data = 'C) Choice MM';
+        $('#choiceD')[0].nextSibling.data = 'D) Choice NN';
+        $("#submit9").hide();
+        $("#submit10").show();
+    
+    });
+});
+//when the 10th question is submitted hide the questions and show the end-screen
+$(document).ready(function(){
+    $('#submit10').click(function(){
+        $("#submit10").hide();
+        $("#questions").addClass("hide");
+        $("#end-screen").show();
+        $("#start-screen p").hide();
+    
+    });
+});
